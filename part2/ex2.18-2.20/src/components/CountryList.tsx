0975b22c629f03c6b354741ab60ad3a604343220
Rubén Chiquin <randreu28@gmail.com>
2023-09-19 21:03:50 +0200

    Completes ex2.19

--- part2/ex2.18-2.20/src/components/CountryList.tsx
+++ part2/ex2.18-2.20/src/components/CountryList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { CountryType } from "../types";
 import Country from "./Country";
 
@@ -6,15 +7,43 @@ type Props = {
 };
 
 export default function CountryList({ countries }: Props) {
+  const [showingCountries, setShowingCountries] = useState<string[]>([]);
+
   if (countries.length === 1) {
     return <Country country={countries[0]} />;
   }
 
   return (
     <ul>
-      {countries.map((country) => (
-        <li key={country.name.common}>{country.name.common}</li>
-      ))}
+      {countries.map((country) => {
+        const isShowing = showingCountries.includes(country.name.common);
+
+        return (
+          <li key={country.name.common}>
+            <span>{country.name.common}</span>
+            <button
+              onClick={() => {
+                if (!isShowing) {
+                  setShowingCountries([
+                    ...showingCountries,
+                    country.name.common,
+                  ]);
+                } else {
+                  setShowingCountries(
+                    [...showingCountries].filter(
+                      (prevCountry) => prevCountry !== country.name.common
+                    )
+                  );
+                }
+              }}
+            >
+              {isShowing ? "hide" : "show"}
+            </button>
+
+            {isShowing && <Country country={country} />}
+          </li>
+        );
+      })}
     </ul>
   );
 }
